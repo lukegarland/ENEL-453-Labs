daf3db72602868c3ab2448484998495309684ae9
aidanjoh <43867551+aidanjoh@users.noreply.github.com>
2019-10-02 14:04:40 -0600

    Added Select Signal to the Test Bench
    -also updated my prelab word document

--- Lab2/tb_voltmeter.vhd
+++ Lab2/tb_voltmeter.vhd
@@ -1,4 +1,4 @@
--- Voltmeter testbench file for prelab
+-- Voltmeter TestBench
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use ieee.numeric_std.all;
@@ -11,23 +11,26 @@ architecture Behavioral of tb_voltmeter is
 component voltmeter is 
     Port ( clk                           : in  STD_LOGIC;
            reset                         : in  STD_LOGIC;
+			  selectSig							  : in STD_LOGIC
            LEDR                          : out STD_LOGIC_VECTOR (9 downto 0);
            HEX0,HEX1,HEX2,HEX3,HEX4,HEX5 : out STD_LOGIC_VECTOR (7 downto 0)
           );
 End component;
 
-signal clk 								:	STD_LOGIC;
-signal reset						 	:	STD_LOGIC;
+signal clk 										:	STD_LOGIC;
+signal reset						 			:	STD_LOGIC;
+signal selectSig								:  STD_LOGIC;
 signal HEX0,HEX1,HEX2,HEX3,HEX4,HEX5 	:	STD_LOGIC_VECTOR (7 downto 0);
 signal LEDR                          	:	STD_LOGIC_VECTOR (9 downto 0);
+
 constant clk_period : time := 10 ns;
 
 begin 
-
 voltmeter_ins: Voltmeter port map(
-									clk		=>		clk ,
-									reset	=>		reset,
-									LEDR	=>		LEDR,
+									clk   =>		   clk ,
+									reset	=>		   reset,
+									selectSig =>   selectSig,
+									LEDR	=>		   LEDR,
 									HEX0	=> 		HEX0,
 									HEX1	=> 		HEX1,
 									HEX2	=> 		HEX2,
@@ -42,6 +45,7 @@ voltmeter_ins: Voltmeter port map(
 			wait for clk_period/2;
 			clk <= '1';
 			wait for clk_period/2;
+			
 	end process;
 	
 	reset_process: process
@@ -49,9 +53,19 @@ voltmeter_ins: Voltmeter port map(
 			reset <= '0';
 			wait for clk_period;
 			reset <= '1';
-			wait for clk_period*4; --hold reset for 4 clock cycles
+			wait for clk_period*4; -- resets for 4 CLK cycles
 			reset <= '0';
 			wait;
+			
 	end process;
+	
+	select_Sig: process
+	begin
+			selectSig <= '0';
+			wait for 50 ns;
+			selectSig <= '1';
+			wait for 100 ns;
+	end process;
+	
 end Behavioral;
 	
