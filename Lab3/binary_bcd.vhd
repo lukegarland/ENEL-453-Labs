8c202068423719c0e8e21c2d70298f90f10cab67
aidanjoh <43867551+aidanjoh@users.noreply.github.com>
2019-10-13 16:00:18 -0600

    Made the NextState a signal vs a variable

--- Lab3/binary_bcd.vhd
+++ Lab3/binary_bcd.vhd
@@ -19,6 +19,7 @@ END binary_bcd;
 ARCHITECTURE behavior OF binary_bcd IS
 type statetype is (S0,S1,S2,S3,S4,S5,s6);
 signal CurentState: statetype:=S0;
+signal NextState: statetype;
 Signal   counter:STD_LOGIC_VECTOR(3 downto 0):=(others=>'0');
 Signal   counter_2: integer:=0;
 Signal   bcd_signal:  unsigned(28 DOWNTO 0):=(others=>'0');    -- 28 _ _ 25    24 _ _ 21    20 _ _ 17    16 _ _ 13    12 _ _ _ _ _ _ _ _ _ _ _ 0 
@@ -29,7 +30,6 @@ Constant add3_3digit: unsigned(28 DOWNTO 0):="00110000000000000000000000000";
 
 begin
 bcd_process: process (reset)
-variable NextState: statetype;
 begin
       if (reset='1') then
          bcd<= (others=>'0');
@@ -42,47 +42,47 @@ begin
                   when S0 =>
                               bcd_signal(12 DOWNTO 0)<=UNSIGNED(binary);
                               
-                              NextState:=S1;                            
+                              NextState <= S1;                            
                   when S1 => 
                               if (bcd_signal(28 downto 25) >4) then
                                  bcd_signal <= bcd_signal+ add3_3digit;
                               end if;
                               
-                              NextState:=S2;
+                              NextState <= S2;
                   when S2 => 
                               if (bcd_signal(24 downto 21) >4) then
                                  bcd_signal <=bcd_signal+ add3_2digit;
                               end if;
 
-                              NextState:=S3;                   
+                              NextState <= S3;                   
                   when S3 => 
                               if (bcd_signal(20 downto 17) >4) then
                                  bcd_signal <= bcd_signal+ add3_1digit;
                               end if;
 
-                              NextState:=S4;
+                              NextState <= S4;
                   when S4 => 
                               if (bcd_signal(16 downto 13) >4) then
                                  bcd_signal <= bcd_signal+ add3_0digit;
                               end if;
 
-                              NextState:=S5;
+                              NextState <= S5;
                   when S5 => 
                               bcd_signal<=shift_left(unsigned(bcd_signal), 1);
-                              NextState:=S6;
+                              NextState <= S6;
                   when S6 =>
                               If (counter_2=12) then
                                  bcd<=std_logic_vector(bcd_signal(28 downto 13));
                                  bcd_signal<=(others=>'0');
                                  counter_2<=0;
-                                 NextState:=S0;
+                                 NextState <= S0;
                               else
                                  counter_2<=counter_2+1;
-                                 NextState:=S1;
+                                 NextState <= S1;
                               end if;
 
                   When others => 
-                              NextState:=S0;
+                              NextState <= S0;
                               counter_2<=0;
                               bcd_signal<=(others=>'0');
                End Case;
@@ -90,7 +90,6 @@ begin
 end process;
 
 synchronousTransitionProcess : process (CLK)
-variable NextState: statetype;
 begin
 	if (rising_edge(clk)) then
 		CurentState <= NextState;
