ba96b86ea1e05cb15abb6d94929aa2c529968db0
aidanjoh <43867551+aidanjoh@users.noreply.github.com>
2019-10-17 10:17:15 -0600

    Prelab Registers Reset Comment Updated

--- Lab3/registers.vhd
+++ Lab3/registers.vhd
@@ -1,10 +1,10 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
---The problem with the reset in this file was that this reset if it is spposed to be synchronous then the if reset = '1' then and the line underneath
--- should be under the rising CLK edge code. (I did changes so that it is synchronous)
+--The problem with the reset in this file was that if the reset was supposed to be synchronous then the line: "if reset = '1' then" and the line underneath
+--should be under the rising CLK edge code. (I made this change so that it is synchronous)
 
--- If it was suppose to be asynchronous then the reset signals needs to be included in the process list as it is constantly checking to see if needs to be reset 
+-- If it was suppose to be asynchronous then the reset signals needs to be included in the process list as the process is constantly checking the value of reset 
 
 entity registers is
 
