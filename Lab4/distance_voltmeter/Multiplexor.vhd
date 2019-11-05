4722dda519c611007a1b24a5e0131cad6f8d95af
lukegarland <luke.garland01@gmail.com>
2019-11-04 21:04:12 -0700

    changed voltmeter to lab 2 version

--- Lab4/distance_voltmeter/Multiplexor.vhd
+++ Lab4/distance_voltmeter/Multiplexor.vhd
@@ -6,9 +6,9 @@ use ieee.numeric_std.all;
 entity Multiplexor is
 	port(
 	 selectLine : in std_logic;
-	 input1 : in std_logic_vector(12 downto 0);
-	 input2 : in std_logic_vector(12 downto 0);
-	 muxOutput : out std_logic_vector(12 downto 0)
+	 input1 : in std_logic_vector(11 downto 0);
+	 input2 : in std_logic_vector(11 downto 0);
+	 muxOutput : out std_logic_vector(11 downto 0)
 	 );
 end Multiplexor;
 
